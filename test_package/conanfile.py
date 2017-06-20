da0484ccc23e37b051d64c605c7b682221988cf5
memsharded <james@conan.io>
2017-06-20 13:50:21 +0200

    updated test_package cmake

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -12,10 +12,10 @@ class JsonForModernCppTestConan(ConanFile):
     generators = "cmake"
 
     def build(self):
-        cmake = CMake(self.settings)
+        cmake = CMake(self)
         # Current dir is "test_package/build/<build_id>" and CMakeLists.txt is in "test_package"
-        cmake.configure(self, source_dir=self.conanfile_directory, build_dir="./")
-        cmake.build(self)
+        cmake.configure(source_dir=self.conanfile_directory, build_dir="./")
+        cmake.build()
 
     def test(self):
         os.chdir("bin")
