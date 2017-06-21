2babeae2d6684618849196938054e960d43c7ea3
memsharded <james@conan.io>
2017-06-21 18:45:05 +0200

    add License

--- conanfile.py
+++ conanfile.py
@@ -1,4 +1,4 @@
-from conans import ConanFile
+from conans import ConanFile, tools
 from conans.tools import download
 
 class JsonForModernCppConan(ConanFile):
@@ -12,8 +12,16 @@ class JsonForModernCppConan(ConanFile):
     def source(self):
         download("https://github.com/nlohmann/json/releases/download/v%s/json.hpp" % self.version, "json.hpp")
 
+    def build(self):
+        # as there is no LICENSE file, lets extract and generate it.
+        # It is useful for package consumers, so they can collect all license from all dependencies
+        tmp = tools.load("json.hpp")
+        license_contents = tmp[2:tmp.find("*/", 1)]
+        tools.save("LICENSE", license_contents)
+
     def package(self):
         self.copy("*.hpp", dst="include")
+        self.copy("LICENSE")
 
     def package_info(self):
         self.cpp_info.libdirs = []
